f737836ec488a5413e8ff6661b3f4c2e4029fa88
zoujiaying <46364248+zoujiaying1995@users.noreply.github.com>
2025-03-09 20:33:26 +0800

    Update with-images.vue

--- docs/examples/card/with-images.vue
+++ docs/examples/card/with-images.vue
@@ -1,6 +1,5 @@
 <template>
   <el-card style="max-width: 480px">
-    <template #header>Yummy hamburger</template>
     <img
       src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
       style="width: 100%"
